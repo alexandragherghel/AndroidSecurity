f507a08120ad22c969d5372795da4be310a302a3
alexandragherghel <72292156+alexandragherghel@users.noreply.github.com>
2022-11-05 09:24:27 +0200

    updates

--- app/src/main/java/com/example/ism2022/Contacte.java
+++ app/src/main/java/com/example/ism2022/Contacte.java
@@ -1,36 +1,27 @@
 package com.example.ism2022;
 
 public class Contacte {
-	
-	String idc;
-	String numec;
-	String telefonc;
-	
-	public Contacte(String id, String num, String tel) {
-		super();
-		this.idc = id;
-		this.numec = num;
-		this.telefonc = tel;
-	}
-	
-	public String getID() {
-		return idc;
-	}
-	public void setID(String id) {
-		this.idc = id;
-	}
-	
-	public String getNume() {
-		return numec;
-	}
-	public void setNume(String num) {
-		this.numec = num;
-	}
-	
-	public String getTelefon() {
-		return telefonc;
-	}
-	public void setTelefon(String tel) {
-		this.telefonc = tel;
-	}
+
+    String idc;
+    String numec;
+    String telefonc;
+
+    public Contacte(String id, String num, String tel) {
+        super();
+        this.idc = id;
+        this.numec = num;
+        this.telefonc = tel;
+    }
+
+    public String getID() {
+        return idc;
+    }
+
+    public String getNume() {
+        return numec;
+    }
+
+    public String getTelefon() {
+        return telefonc;
+    }
 }
--- app/src/main/java/com/example/ism2022/ContactsList.java
+++ app/src/main/java/com/example/ism2022/ContactsList.java
@@ -3,34 +3,29 @@ package com.example.ism2022;
 import java.util.ArrayList;
 
 public class ContactsList {
-	
-	static ArrayList<Contacte> lista = new
-            ArrayList<Contacte>(100);
-	
-	public ContactsList()
-	{
-		
-	}
 
-	public void adaugaContact(Contacte c) 
-	{
-		lista.add(c);
-	}
-	
-	public String toString()
-	{
-		StringBuilder contact = new StringBuilder();
-		
-		for(Contacte c:lista)
-		{
-			contact.append(c.getID());
-			contact.append(" ");
-			contact.append(c.getNume());
-			contact.append(" ");
-			contact.append(c.getTelefon());
-			contact.append(" ");
-			contact.append("\n");
-		}
-		return contact.toString();	
-	}
+    static ArrayList<Contacte> lista = new ArrayList<Contacte>(100);
+
+    public ContactsList() {
+
+    }
+
+    public void adaugaContact(Contacte c) {
+        lista.add(c);
+    }
+
+    public String toString() {
+        StringBuilder contact = new StringBuilder();
+
+        for (Contacte c : lista) {
+            contact.append(c.getID());
+            contact.append(" ");
+            contact.append(c.getNume());
+            contact.append(" ");
+            contact.append(c.getTelefon());
+            contact.append(" ");
+            contact.append("\n");
+        }
+        return contact.toString();
+    }
 }
